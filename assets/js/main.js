860c18f5e0cde11103963ac9dfded879807ee54b
StephanPagel <stephan.pagel84@gmail.com>
2022-04-20 18:29:39 +0200

    finished

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+let headline = document.getElementById('headline');
+
+function bgGreen() {
+    document.body.style.backgroundColor = 'rgba(0, 140, 9, 1)';
+    headline.style.color = 'rgba(117, 1, 129, 1)';
+}
+
+function bgPurple() {
+    document.body.style.backgroundColor = 'rgba(79, 0, 128, 1)';
+    headline.style.color = 'rgba(253, 187, 45, 1)';
+}
+
+function bgBlack() {
+    document.body.style.backgroundColor = 'rgba(0, 0, 0, 1)';
+    headline.style.color = 'rgba(252, 155, 70, 1)';
+}
+
+function bgBlue() {
+    document.body.style.backgroundColor = 'rgba(26, 0, 175, 1)';
+    headline.style.color = 'rgba(65, 238, 255, 1)';
+}
